dfff69ac22278cf76a508c8c4f0d06c28086cd4b
I Temply <bluerambles278@gmail.com>
2024-06-04 16:17:44 +0000

    SQL Setup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const dotenv = require('dotenv');
+const mysql = require('mysql');
 dotenv.config()
 
 app.set('views', path.join(__dirname, 'public'))
@@ -13,10 +14,15 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res){
+const databaseUrl = process.env.DATABASE_URL
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
+const port = process.env.PORT
+
+app.get('/', async function(req, res){
 
 })
 
 server.listen(3000, () => {
-  console.log('092Chat started on port 3000.')
+  console.log('092Chat started on port 3000. https://127.0.0.1:3000/')
 })
