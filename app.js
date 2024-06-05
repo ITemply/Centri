74f41817856d3cdab5d3c667e5bcc6938f462cbe
ITemply <120138779+ITemply@users.noreply.github.com>
2024-06-05 02:09:46 +0000

    Sign ups, Still a work in progress.

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const username = process.env.USERNAME
 const password = process.env.PASSWORD
 const port = process.env.PORT
 
+const algorithm = process.env.ALG
+const key = process.env.KEY
+
+const validChars = process.env.VALID_CHARS
+const validUsernameChars = process.env.VALID_UNAME_CHARS
+
 async function executeSQL(sql){
   var connection = mysql.createConnection({
       host: databaseUrl,
@@ -43,10 +49,82 @@ async function executeSQL(sql){
   })
 }
 
-app.get('/', async function(req, res){
+function hash(string) {
+    return createHash('sha256').update(string).digest('hex');
+}
 
+function base64Image(file){
+    return fs.readFileSync(file, 'base64')
+}
+
+function checkUserane(inputString) {
+    const characterList = new RegExp(`[^${validUsernameChars}]`, 'g');
+    return !characterList.test(inputString);
+}
+
+function encode(text) {
+    const cipher = crypto.createCipher(algorithm, key);  
+    const  encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+    return encrypted
+}
+
+function decode(text) {
+    const  decipher = crypto.createDecipher(algorithm, key);
+    const decrypted = decipher.update(encrypted, 'hex', 'utf8') + decipher.final('utf8');
+    return decrypted
+}
+
+function checkCharacters(inputString) {
+    const characterList = new RegExp(`[^${validChars}]`, 'g');
+    return !characterList.test(inputString);
+}
+
+app.get('/', async function(req, res){
+    console.log(await executeSQL('SHOW TABLES IN Centri;'))
+})
+
+app.post('/newsignup', async function(req, res){
+    console.log('Sending [POST]: New Sign Up')
+    const jsonData = req.body
+    let username = jsonData.username
+    let password = jsonData.password
+    let checkUsername = username.toLowerCase()
+    let newPassword = hash(password)
+    let token = hash(checkUsername) + '.' + newPassword
+
+    let profileIcon = base64Image(workingDirectory + '/media/accounts/ProfileIcon.png')
+    let profileBanner = base64Image(workingDirectory + '/media/accounts/ProfileBanner.png')
+
+    if (await checkCurrentUsername(checkUsername)) {
+        if (checkUsername.length == 0) {
+            res.send(JSON.stringify({information: 'A Username Is Required'}))
+            return
+        }
+        if (checkUsername != null && checkUsername.length < 25) {
+            if (checkCharacters(checkUsername)) {
+                if (password.length > 0) {
+                    //await executeSQL("INSERT INTO accounting (username, checkUsername, password, token, status) VALUES ('" + username + "', '" + checkUsername + "', '" + newPassword + "', '" + token + "', 'User');")
+                    //await executeSQL("INSERT INTO profileData (pfp, banner, description) VALUES ('" + profileIcon + "', '" + profileBanner + "', 'A new Temeka user!');")
+                    res.send(JSON.stringify({information: 'Signed Up', token: token}))
+                    return
+                } else {
+                    res.send(JSON.stringify({information: 'A Password Is Required'}))
+                    return
+                }
+            } else {
+                res.send(JSON.stringify({information: 'User Has Invalid Letters'}))
+                return
+            }
+        } else {
+            res.send(JSON.stringify({information: 'User Is Too Big'}))
+            return
+        }
+    } else {
+        res.send(JSON.stringify({information: 'User Already Taken'}))
+        return
+    }
 })
 
 server.listen(3000, () => {
-  console.log('092Chat started on port 3000. https://127.0.0.1:3000/')
+  console.log('Centri started on port 3000. https://127.0.0.1:3000/')
 })
