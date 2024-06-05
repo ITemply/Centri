8152e8052f4d946dc31ef21e43b6ebe6a7bea91a
ITemply <120138779+ITemply@users.noreply.github.com>
2024-06-05 11:35:05 +0000

    Sign Up

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const path = require('node:path')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const app = express()
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const dotenv = require('dotenv');
-const mysql = require('mysql');
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const dotenv = require('dotenv')
+const mysql = require('mysql')
 dotenv.config()
 
 app.set('views', path.join(__dirname, 'public'))
@@ -49,8 +49,22 @@ async function executeSQL(sql){
   })
 }
 
+function randomString(length) {
+    let result = ''
+    const characters = validChars
+    const charactersLength = characters.length
+    let counter = 0
+
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      counter += 1
+    }
+
+    return result
+}
+
 function hash(string) {
-    return createHash('sha256').update(string).digest('hex');
+    return createHash('sha256').update(string).digest('hex')
 }
 
 function base64Image(file){
@@ -58,29 +72,31 @@ function base64Image(file){
 }
 
 function checkUserane(inputString) {
-    const characterList = new RegExp(`[^${validUsernameChars}]`, 'g');
-    return !characterList.test(inputString);
+    const characterList = new RegExp(`[^${validUsernameChars}]`, 'g')
+    return !characterList.test(inputString)
 }
 
-function encode(text) {
-    const cipher = crypto.createCipher(algorithm, key);  
-    const  encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+function encode(text, algkey) {
+    const cipher = crypto.createCipher(algorithm, algkey)  
+    const  encrypted = cipher.update(text, 'utf8', 'hex') + cipher.final('hex')
     return encrypted
 }
 
-function decode(text) {
-    const  decipher = crypto.createDecipher(algorithm, key);
-    const decrypted = decipher.update(encrypted, 'hex', 'utf8') + decipher.final('utf8');
+function decode(text, algkey) {
+    const  decipher = crypto.createDecipher(algorithm, algkey)
+    const decrypted = decipher.update(text, 'hex', 'utf8') + decipher.final('utf8')
     return decrypted
 }
 
 function checkCharacters(inputString) {
-    const characterList = new RegExp(`[^${validChars}]`, 'g');
-    return !characterList.test(inputString);
+    const characterList = new RegExp(`[^${validChars}]`, 'g')
+    return !characterList.test(inputString)
 }
 
 app.get('/', async function(req, res){
+    console.log('Sending [GET]: /')
     console.log(await executeSQL('SHOW TABLES IN Centri;'))
+    res.render('index')
 })
 
 app.post('/newsignup', async function(req, res){
@@ -89,22 +105,20 @@ app.post('/newsignup', async function(req, res){
     let username = jsonData.username
     let password = jsonData.password
     let checkUsername = username.toLowerCase()
-    let newPassword = hash(password)
-    let token = hash(checkUsername) + '.' + newPassword
+    let hashedPassword = hash(password)
+    let hashedCheckUsername = hash(checkUsername)
+    let token = hashedCheckUsername+ '.' + newPassword
+    let encodedUsername = encode(username, key)
 
-    let profileIcon = base64Image(workingDirectory + '/media/accounts/ProfileIcon.png')
-    let profileBanner = base64Image(workingDirectory + '/media/accounts/ProfileBanner.png')
-
-    if (await checkCurrentUsername(checkUsername)) {
+    if (await checkCurrentUsername(hashedCheckUsername)) {
         if (checkUsername.length == 0) {
             res.send(JSON.stringify({information: 'A Username Is Required'}))
             return
         }
-        if (checkUsername != null && checkUsername.length < 25) {
+        if (checkUsername != null && checkUsername.length < 20) {
             if (checkCharacters(checkUsername)) {
                 if (password.length > 0) {
-                    //await executeSQL("INSERT INTO accounting (username, checkUsername, password, token, status) VALUES ('" + username + "', '" + checkUsername + "', '" + newPassword + "', '" + token + "', 'User');")
-                    //await executeSQL("INSERT INTO profileData (pfp, banner, description) VALUES ('" + profileIcon + "', '" + profileBanner + "', 'A new Temeka user!');")
+                    await executeSQL("INSERT INTO accounting (username, checkUsername, password, token, status) VALUES ('" + encodedUsername + "', '" + hashedCheckUsername + "', '" + hashedPassword + "', '" + token + "', 0);")
                     res.send(JSON.stringify({information: 'Signed Up', token: token}))
                     return
                 } else {
